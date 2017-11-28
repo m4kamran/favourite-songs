f3f53c159334f7bb8bba1632e7961cb3b6c06ab2
Muhammad Kamran <Kamran@Kamrans-MacBook-Air.local>
2017-11-29 01:09:01 +0500

    Setting up the registration page

--- server/src/app.js
+++ server/src/app.js
@@ -11,8 +11,8 @@ app.use(cors())
 
 app.listen(process.env.PORT || 8081)
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
     res.send({
-        message: 'Hello world!'
+        message: `Hello ${req.body.email}! your user was registered.`
     })
 })
